1ce98a74794c60d821973d1c5689f4002404e544
julianxlozano <julianxlozano@gmail.com>
2021-07-06 14:38:00 -0500

    commit tree 946279c9df22ada8040acef2227b3d46ed7c3250

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,49 @@
 // Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
 
 class BandInput extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch({ type: 'ADD_BAND', payload: this.state });
+    this.setState({
+      text: '',
+    })
+  };
+
+
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>Add band</label>
+            <input
+          type="text"
+          onChange={this.handleChange} value={this.state.text}/>
+          </p>
+          <input type="submit" />
+       </form>
+       {this.state.text}
       </div>
     )
   }
 }
 
-export default BandInput
+export default connect()(BandInput); 
